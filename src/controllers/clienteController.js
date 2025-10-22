36a6a88fcad66e152bc8b3461dd09f89e1794643
livolario <livolario@gmail.com>
2025-10-22 17:19:33 -0300

    Add db

---
+++ src/controllers/clienteController.js
@@ -0,0 +1,91 @@
+//Usamos 'import * as' para agrupar todas as exportações do service
+import * as clienteService from '../services/clienteService.js';
+import Joi, { allow } from 'joi';
+
+//Usando 'export const'
+export const clienteCreateSchema = Joi.object({
+    cpf: Joi.string().length(11).required(),
+    nome: Joi.string().required().max(100),
+    endereco: Joi.string().required().max(100),
+    bairro: Joi.string().max(30).allow(''),
+    cidade: Joi.string().max(30).allow(''),
+    cep: Joi.string().required(),
+    telefone: Joi.string().required(),
+    email: Joi.string().email().required().max(30),
+    senha: Joi.string().min(6).required(),
+});
+
+export const clienteUpdateSchema = Joi.object({
+    nome: Joi.string().max(100),
+    endereco: Joi.string().max(100),
+    bairro: Joi.string().max(30).allow(''),
+    cidade: Joi.string().max(30).allow(''),
+    cep: Joi.string(),
+    telefone: Joi.string(),
+    email: Joi.string().email().max(30),
+    senha: Joi.string().min(6),
+}).min(1);
+
+export const listarClientes = async(req, res) => {
+    try {
+        const clientes = await clienteService.findAll();
+        res.status(200).json(clientes);
+    } catch (err) {
+        console.error('Erro ao buscar clientes:', err)
+        res.status(500).json({ error: 'Erro interno do servidor'});
+    }
+};
+
+export const listarClienteCpf = async (req, res) => {
+    try{
+        const{cpf} = req.params;
+        const cliente = await clienteService.findByCpf(cpf);
+        if (!cliente) {
+            return res.status(404).json({error: 'Cliente não encontrado'});
+        }
+        res.status(200).json(cliente);
+    } catch (err) {
+        console.error('Erro ao buscar client:', err);
+        res.status(500).json({error: 'Erro interno do servidor'});
+    }
+};
+
+export const adicionarCliente = async (req, res) => {
+    try {
+        const novoCliente = await clienteService.create(req.body);
+        res.status(201).json({message: 'Cliente adicionado com sucesso', data: novoCliente});
+    } catch (err) {
+        console.error('Erro ao adicionar cliente:', err);
+        if (err.code === 'ER_DUP_ENTRY') {
+            return res.status(409).json({error:'CPF já cadastrado.'})
+        }
+        res.status(500).json({error: 'Erro ao adicionar cliente'});
+    }
+};
+
+export const atualizarCliente = async (req, res) => {
+    try {
+        const { cpf } = req.params;
+        const update = await clienteService.update(cpf, req.body);
+        if (!updated) {
+            return res.status(404).json({ error: 'Cliente não encontrado'});
+        }
+        res.status(200).json({message: 'Cliente atualizado com sucesso'});
+    } catch (err) {
+        console.error('Erro ao atualizar cliente:', err);
+        res.status(500).json({error: 'Erro ao atualizar cliente'})
+    }
+};
+
+export const deletarCliente = async (req, res) => {
+    try{
+        const { cpf } = req.params;
+        const deleted = await clienteService.remove(cpf);
+        if (!deleted) {
+            return res.status(404).json({error: 'Cliente não encontrado'})
+        }
+    } catch (err) {
+        console.error('Erro ao deletar cliente:', err);
+        res.status(500).json({error: 'Erro ao deletar cliente'});
+    }
+};
---
+++ src/services/clienteService.js
@@ -0,0 +1,51 @@
+//Sintaxe de 'import'. Note a extensão'.js' no import relativo
+import db from '../db/db.js';
+import bcrypt from 'bcrypt';
+
+//Usando 'export const' para exportações nomeadas
+export const findAll = async () => {
+    const [result] = await db.query('SELECT * FROM cliente');
+    return result;
+};
+
+export const findByCpf = async (cpf) => {
+    const [result] = await db.query('SELECT * FROM cliente WHERE cpf = ?', [cpf]);
+    return result.length > 0 ? result[0] : null;
+};
+
+export const findByEmail = async (email) => {
+    const sql = 'SELECT * FROM cliente WHERE email = ?';
+    const [rows] = await db.query(sql, [email]);
+
+    //Retorna o primeiro cliente encontrado ou null
+    return rows[0] || null;
+};
+
+export const create = async (clienteData) => {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(clienteData.senha,saltRounds)
+    const newCliente = {
+        ...clienteData,
+        senha: hashedPassword,
+    };
+
+    await db.query('INSERT INT cliente SET ?', newCliente);
+
+    delete newCliente.senha;
+    return newCliente;
+};
+
+export const update = async (cpf, clienteData) => {
+    if (clienteData.senha) {
+        const saltRounds = 10;
+        clienteData.senha = await bcrypt.hash(clienteData.senha,saltRounds);
+    }
+    const [result] = await db.query('UPDATE cliente SET ? WHERE cpf = ?',[clienteData, cpf]);
+    return result.affectedRows > 0;
+    
+};
+
+export const remove = async (cpf) => {
+    const [result] = await db.query('DELETE FROM cliente WHERE cpf = ?', [cpf]);
+    return result.affectedRows > 0;
+};
